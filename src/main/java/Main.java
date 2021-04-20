638f1580cbfadd341150d4023b0c0b46aed5f14f
Donnerdistel <hartmannjs@icloud.com>
2021-04-20 09:38:16 +0200

    Farbe upgedatet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Factory factory = new Factory();
 
-        Car car = new Car("red", "BMW", 5);
+        Car car = new Car("yellow", "BMW", 5);
         Car car1 = new Car("blue", "Audi", 3);
 
         factory.buildCar(car);
